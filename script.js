4118bf0fd23f405509caa60437635dbeeec74eda
Tramio <tramiodev@outlook.fr>
2021-10-03 22:01:14 +0200

    Create functions to assess player1 victory

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const Gameboard = (() => {
                 currentPlayer = Game.getCurrentTurn();
                 square.classList.add(currentPlayer.marker);
                 theArray[square.dataset.value] = currentPlayer.marker;
+                assessAllPlayerOneWins();
                 Game.setNewTurn();
             }, { once: true });
         }
@@ -54,14 +55,17 @@ const Game = (() => {
 })();
 Game.start();
 
-// Si les signes sont identiques pour :
-    // winningCombinations
-        // [1, 2, 3]
-        // [4, 5, 6]
-        // [7, 8, 9]
-        // [1, 4, 7]
-        // [2, 5, 8]
-        // [3, 6, 9]
-        // [1, 5, 9]
-        // [3, 5, 7]
-// alors regarde le signe, chope le player correspondant et fais-le win
+function isPlayerOneWin([a, b, c]) {
+    if (theArray[a] === player1.marker && theArray[a] == theArray[b] && theArray[a] == theArray[c])
+    { return true } else { return false };
+}
+
+const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+function assessAllPlayerOneWins() {
+    winningCombinations.forEach(combination => {
+        if (isPlayerOneWin(combination)) {
+            console.log("Player 1 won!");
+        };
+    });
+}
