0b40a4e8e575ede33e3e281140184750594dbcf9
Tramio <tramiodev@outlook.fr>
2021-10-04 11:34:45 +0200

    Store global functions in respective module (Game)

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const Gameboard = (() => {
         currentPlayer = Game.getCurrentTurn();
         e.target.classList.add(currentPlayer.marker);
         theArray[e.target.dataset.value] = currentPlayer.marker;
-        checkWin();
+        Game.checkWin();
         Game.setNewTurn();
     }
     return {
@@ -39,6 +39,7 @@ const player2 = createPlayer("Anglerfish", "O");
 
 const Game = (() => {
     let _currentPlayer = player1;
+    const _winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     const start = () => {
         theArray = Gameboard.create(3);
         Gameboard.display();
@@ -49,32 +50,28 @@ const Game = (() => {
     const setNewTurn = () => {
         _currentPlayer === player1 ? _currentPlayer = player2 : _currentPlayer = player1;
     }
+    const _isWin = ([a, b, c]) => {
+        return (theArray[a] == player1.marker || theArray[a] == player2.marker) && theArray[a] == theArray[b] && theArray[a] == theArray[c];
+    }
+    const _getWinner = ([a, b, c]) => {
+        theArray[a] == player1.marker ? console.log("player1 won!") : console.log("player2 won!");
+    }
+    const checkWin = () => {
+        _winningCombinations.forEach(combination => {
+            if (_isWin(combination)) {
+                _getWinner(combination);
+                const remainingSquares = Array.from(document.querySelectorAll(".square"));
+                remainingSquares.forEach(square => {
+                    square.removeEventListener("click", Gameboard.displayCurrentMarker, { once: true });
+                })
+            };
+        });
+    }
     return {
         start,
         getCurrentTurn,
         setNewTurn,
+        checkWin,
     };
 })();
 Game.start();
-
-const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
-function isWin([a, b, c]) {
-    return (theArray[a] == player1.marker || theArray[a] == player2.marker) && theArray[a] == theArray[b] && theArray[a] == theArray[c];
-}
-
-function getWinner([a, b, c]) {
-    theArray[a] == player1.marker ? console.log("player1 won!") : console.log("player2 won!");
-}
-
-function checkWin() {
-    winningCombinations.forEach(combination => {
-        if (isWin(combination)) {
-            getWinner(combination);
-            const remainingSquares = Array.from(document.querySelectorAll(".square"));
-            remainingSquares.forEach(square => {
-                square.removeEventListener("click", Gameboard.displayCurrentMarker, { once: true });
-            })
-        };
-    });
-}
